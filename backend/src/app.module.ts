5f5bdebbc7bd43238cec2540de5472d70e20812d
Ebooth <pauldejean01@gmail.com>
2021-03-30 14:39:17 +0530

    Moved config and built backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CharactersModule } from './characters/characters.module';
-import { configService } from './config/config.service';
+import { configService } from '../config/config.service';
 
 @Module({
   imports: [CharactersModule, TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
